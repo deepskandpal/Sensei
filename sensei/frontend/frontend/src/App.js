e6bdd21495bdd765a47fceca26dee40cd46017aa
Deepanshu Kandpal <dkandpalz@gmail.com>
2021-05-22 18:08:57 +0530

    update in App.js

--- sensei/frontend/frontend/src/App.js
+++ sensei/frontend/frontend/src/App.js
@@ -2,32 +2,6 @@ import React, { Component } from "react";
 import Modal from "./components/Modal";
 import axios from "axios";
 
-const todoItems = [
-  {
-    id: 1,
-    title: "Go to Market",
-    description: "Buy ingredients to prepare dinner",
-    completed: true,
-  },
-  {
-    id: 2,
-    title: "Study",
-    description: "Read Algebra and History textbook for the upcoming test",
-    completed: false,
-  },
-  {
-    id: 3,
-    title: "Sammy's books",
-    description: "Go to library to return Sammy's books",
-    completed: true,
-  },
-  {
-    id: 4,
-    title: "Article",
-    description: "Write article on how to use Django with React",
-    completed: false,
-  },
-];
 
 class App extends Component {
   constructor(props) {
@@ -55,7 +29,7 @@ class App extends Component {
       .catch((err) => console.log(err));
   };
 
-  
+
   toggle = () => {
     this.setState({ modal: !this.state.modal });
   };
